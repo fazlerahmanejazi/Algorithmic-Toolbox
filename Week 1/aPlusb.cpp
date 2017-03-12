3fe03e87ff52f64198944b77a7815da21d3567b0
Fazle Rahman Ejazi <ahmed.belal98@gmail.com>
2017-03-13 04:15:41 +0530

    Add files via upload

---
+++ Week 1/aPlusb.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main()
+  {
+    ios_base::sync_with_stdio (false) ;
+    int a = 0, b = 0, sum = 0 ;
+    cin >> a ;
+    cin >> b ;
+    sum = a + b ;
+    cout << sum ;
+    return 0 ;
+  }
